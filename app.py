55d554528adcde6bdd89210deb85076cc1f2733e
jicbo <bojicmarko06@gmail.com>
2025-03-16 17:19:38 +0100

    Switched to Postgres

--- app.py
+++ app.py
@@ -102,6 +102,6 @@ def create_test_users():
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+        db.create_all()  # Create the database schema
         create_test_users()
         app.run(debug=True)
--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
+    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///hospital.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://neondb_owner:npg_NarPwj8WyGp7@ep-nameless-lab-a2ofntv0-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # Email configuration
