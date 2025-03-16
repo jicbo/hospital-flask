10cbe94c98849f7e9dcec4a017b3db57991d6227
jicbo <bojicmarko06@gmail.com>
2025-03-16 17:27:14 +0100

    z

--- config.py
+++ config.py
@@ -1,17 +1,6 @@
 import os
 
 class Config:
-    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://neondb_owner:npg_NarPwj8WyGp7@ep-nameless-lab-a2ofntv0-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Email configuration
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', True)
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', False)
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
-    ADMINS = ['your-email@example.com']
